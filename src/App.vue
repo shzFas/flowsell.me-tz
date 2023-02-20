b41a653c1d8e59256bc45da97de6639fd5a4829b
shzfas <yuraka200486@gmail.com>
2023-02-20 15:55:51 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { VueFlow } from '@vue-flow/core'
+import { ref } from 'vue'
+import SaveRestoreControls from './components/Controls.vue';
+import { Controls } from '@vue-flow/controls'
+import './style.css';
+
+const elements = ref([{ id: '1', label: 'Node 1', position: { x: 100, y: 100 } }])
+</script>
+
+<template>
+  <VueFlow v-model="elements">
+    <SaveRestoreControls />
+    <Controls />
+  </VueFlow>
+</template>
---
+++ src/components/Controls.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { Panel, PanelPosition, useVueFlow } from '@vue-flow/core'
+
+const flowKey = 'example-flow'
+
+const { nodes, addNodes, setNodes, setEdges, dimensions, setTransform, toObject, addEdges, onConnect } = useVueFlow()
+
+onConnect((params) => addEdges([params]))
+
+const onSave = () => {
+  localStorage.setItem(flowKey, JSON.stringify(toObject()));
+  console.log(localStorage);
+}
+
+const onRestore = () => {
+  const flow = JSON.parse(localStorage.getItem(flowKey))
+
+  if (flow) {
+    const [x = 0, y = 0] = flow.position
+    setNodes(flow.nodes)
+    setEdges(flow.edges)
+    setTransform({ x, y, zoom: flow.zoom || 0 })
+  }
+}
+
+const onAdd = () => {
+  const id = nodes.value.length + 1
+
+  const newNode = {
+    id: `random_node-${id}`,
+    label: `Node ${id}`,
+    position: { x: 0 * dimensions.value.width, y: 0 * dimensions.value.height },
+  }
+
+  addNodes([newNode])
+}
+</script>
+
+<template>
+  <Panel :position="PanelPosition.TopRight" class="save-restore-controls">
+    <button style="background-color: #33a6b8" @click="onSave">save</button>
+    <button style="background-color: #113285" @click="onRestore">restore</button>
+    <button style="background-color: #6f3381" @click="onAdd">add node</button>
+  </Panel>
+</template>
